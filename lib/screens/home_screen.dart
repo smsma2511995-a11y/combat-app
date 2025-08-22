8c49b1d37a8a2a0f559b5a8a0eeacedcdacb69ad
smsma2511995-a11y <smsma2511995@gmail.com>
2025-08-22 22:59:29 +0300

    Create home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'training_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fight Training"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.orange,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+          ),
+          child: Text("ابدأ التمرين", style: TextStyle(fontSize: 20, color: Colors.black)),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => TrainingScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
