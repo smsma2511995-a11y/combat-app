341f3f052a6a4efc10782450fefc6bad82655512
smsma2511995-a11y <smsma2511995@gmail.com>
2025-08-22 23:11:59 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,3 +27,32 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+import 'package:flutter/material.dart';
+import 'exercises_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fight Training"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.orange,
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+          ),
+          child: Text("قائمة التمارين", style: TextStyle(fontSize: 20, color: Colors.black)),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => ExercisesScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
